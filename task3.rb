f7e9cad2aaaf60a30e7b125aca7d5f38412d1b98
ilira5564 <ilira5564@gmail.com>
2018-06-16 14:38:40 +0600

    task 3

---
+++ task3.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+
+while a != 0 && b != 0
+  if a > b
+    a = a % b
+  else
+    b = b % a
+  end
+end
+if a.zero?
+  puts b
+else
+  puts a
+end
